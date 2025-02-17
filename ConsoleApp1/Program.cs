d4bc8becd059e3e3877fa5de155e64a45e8336d8
Riyy37 <domabruho123@gmail.com>
2025-02-17 21:01:46 +0200

    Task1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mas = new int[5];
+            Console.Write("mas:");
+            foreach (int item in mas)
+            {
+                Console.Write(item + " ");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
