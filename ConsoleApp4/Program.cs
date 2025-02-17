0d8511b4c9f46e3ffb42d2ecf49dfba3adafacc4
Riyy37 <domabruho123@gmail.com>
2025-02-17 21:04:01 +0200

    Task4

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] mas = new int[5];
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = random.Next();
+            }
+            Console.Write("mas:");
+            foreach (int item in mas)
+            {
+                Console.Write(item + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
